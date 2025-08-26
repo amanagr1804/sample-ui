cd0e5879449a5b4f6a3874462fdb5e20c1119460
amanagr1804 <117620118+amanagr1804@users.noreply.github.com>
2025-08-27 01:04:06 +0530

    Update app2.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ export function App() {
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is -------------tgergrgr {count}
+          count is -------------tgergrgrsdkjnjnsjndkjsndkjs {count}
         </button>
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
