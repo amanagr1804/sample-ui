afdc0724b3fcc71a6622b681c106dbd3e061211e
amanagr1804 <117620118+amanagr1804@users.noreply.github.com>
2025-08-28 02:08:34 +0530

    Update app.jsx11231232

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ export function App() {
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is "Click me111111111122fff22" {count}
+          count is "Click me1111111ddd11122fff22" {count}
         </button>
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
