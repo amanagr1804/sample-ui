f16773968350815abba6ec3eef1e99072d7a0947
amanagr1804 <117620118+amanagr1804@users.noreply.github.com>
2025-08-30 01:45:09 +0530

    Update app.jsx3232

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ export function App() {
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is "Click me1111111ddd11122fff22" {count} dkjfndsjkdnfksd TESTING WEBHOOK NEW 1222334444fdfdfdsfsdfsdfsdfdjghjjh
+          count is "Click me1111111ddd11122fff22" {count} dkjfndsjkdnfksd TESTING WEBHOOK NEW 
         </button>
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
