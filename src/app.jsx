4c1b4148072b87e0f4abd447cadf82e9e9295696
amanagr1804 <117620118+amanagr1804@users.noreply.github.com>
2025-08-30 15:17:18 +0530

    1234567890

--- src/app.jsx
+++ src/app.jsx
@@ -19,7 +19,7 @@ export function App() {
       <h1>Vite + Preact</h1>
       <div class="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count} AMAN TESTING
+          count is {count} AMAN TESTING 12345
         </button>
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
